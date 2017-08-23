767a7fc1ad27b6f512730e19bd8736ff337d441b
ScriptTiger <thescripttiger@gmail.com>
2017-08-23 14:03:50 +0800

    Repo initialized

---
+++ Brutecycle.cmd
@@ -0,0 +1,77 @@
+@echo off
+
+rem =====
+rem For more information on ScriptTiger and more ScriptTiger scripts visit the following URL:
+rem https://scripttiger.github.io/
+rem Or visit the following URL for the latest information on this ScriptTiger script:
+rem https://github.com/ScriptTiger/Brutecycle
+rem =====
+
+setlocal ENABLEDELAYEDEXPANSION
+
+cd %~dp0
+
+set hex=
+set current=0
+set progress=0
+set total=0
+
+if "%~1"=="" (set /p digits=How many digits to cycle? ) else set digits=%~1
+
+if "%~2"=="" (
+set characters=0 1 2 3 4 5 6 7 8 9 a b c d e f
+echo Current arrangement of default characters if none are specified:
+echo !characters!
+set /p characters=Include what characters in what order? 
+) else set characters=%~2
+
+if "%~3"=="" (
+set output="con"
+echo If no file is specified or "con" is input, the results are standard output
+set /p output=File to ouput results to? 
+) else set output=%~3
+
+if "%~4"=="" (
+set interactive=1
+choice /m "Would you like to display progress?"
+set display=!errorlevel!
+) else (
+set interactive=0
+set display=%~4
+)
+
+for %%0 in (%characters%) do set /a total=!total!+1
+
+if %output%=="con" (call :Cycle) else (
+	for /f "tokens=*" %%0 in ('echo !output!') do @set output="%%~0"
+	call :Cycle > %output%
+)
+
+if "%interactive%"=="1" (
+	echo Process complete.
+	pause
+)
+exit /b
+
+:Cycle
+for %%0 in (%characters%) do (
+	set hex=!hex!%%0
+	set previous=!current!
+	set /a current=!current!+1
+	if !current! gtr !previous! (
+		if !current!==!digits! (
+			echo !hex!
+		) else (
+			call :Cycle
+		)
+	)
+	if !display!==1 if !current!==1 (
+		set /a progress=!progress!+1
+		set /a percent=!progress!00/!total!
+		echo Percent Complete: !percent!%% > con
+	)
+	set hex=!hex:~,-1!
+	set previous=!current!
+	set /a current=!current!-1
+)
+exit /b
---
+++ README.md
@@ -0,0 +1,13 @@
+# Brutecycle
+Cycles through any given character set in any given order and outputs to standard output or to a file. For larger data sets, like 128-bit MD5 hashes, 160-bit SHA-1, 256-bit SHA-256, etc., it is recommended to only do a couple character combinations at a time, unless you plan on having the script run for days on end uninterrupted.
+
+Can run interactively or scripted and with or without progress updates (silent or not).
+
+To output all possible hexidecimal byte combinations with no progress to the console:  
+Brutecycle.cmd 2 "0 1 2 3 4 5 6 7 8 9 a b c d e f" con 0
+
+To output all possible octal byte combinations with no progress to the console:  
+Brutecycle.cmd 3 "0 1 2 3 4 5 6 7" con 0
+
+To output all possible binary byte combinations with no progress to the console:  
+Brutecycle.cmd 8 "0 1" con 0
